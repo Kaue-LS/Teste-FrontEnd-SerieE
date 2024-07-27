0e56338a5a936b1d8d5ca881050da885d7b82137
Kaue-LS <kaue.ls0506@gmail.com>
2024-07-27 11:51:56 -0300

    feat: add home page, pastes and configs

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Home from './pages/home';
+
+function App() {
+  return (
+    <div className="App">
+      Hello World
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
