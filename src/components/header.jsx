bf140eb6d6f2b7f575320f8d69835d1bfc97180f
Kaue-LS <kaue.ls0506@gmail.com>
2024-07-28 00:36:08 -0300

    style: header desktop

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
-import logo1 from "../assets/logo/Group.png";
+import logo1 from "../assets/logo/Group@3x.png";
 export default function Header() {
   return (
-    <header>
-      <div>
+    <header className="header">
+      <div className="header-image">
         <img src={logo1} alt="Marvel Search Heroes" />
       </div>
-      <div>
+      <div className="header-title">
         <h1>EXPLORE O UNIVERSO</h1>
-        <p>
+        <h4>
           Mergulhe no dominio deslumbrante de todos os personagens cássicos que
           você ama - e aqueles que você descobrirá em breve!
-        </p>
+        </h4>
       </div>
     </header>
   );
