f7228a39b075f51337c3a085d6cb23810687902e
Kaue-LS <kaue.ls0506@gmail.com>
2024-07-28 16:17:57 -0300

    style: heroesList Desktop and Mobile

--- src/components/heroesList.jsx
+++ src/components/heroesList.jsx
@@ -1,37 +1,37 @@
 import React from "react";
-import FavoriteIcon from "../assets/icons/favoriteIcon";
+import FavoriteEmptyIcon from "../assets/icons/favoriteEmptyIcon";
 import { Link } from "react-router-dom";
 
 export default function HeroesList({ data }) {
   console.log(data);
-  return data ? (
-    <section>
-      {data.map((item) => {
-        return (
-          <div key={item.id}>
-            <div>
-              <Link to={`/${item.id}`}>
-                <img
-                  src={
-                    item.thumbnail.path +
-                    "/portrait_xlarge." +
-                    item.thumbnail.extension
-                  }
-                  alt={item.name}
-                />
-              </Link>
+  return (
+    <section className="heroesList">
+      <div className="content">
+        {data.map((item) => {
+          return (
+            <div key={item.id} className="card">
+              <div className="image">
+                <Link to={`/${item.id}`}>
+                  <img
+                    src={
+                      item.thumbnail.path +
+                      "/portrait_xlarge." +
+                      item.thumbnail.extension
+                    }
+                    alt={item.name}
+                  />
+                </Link>
+              </div>
+              <div className="nameIcon">
+                <Link to={`/${item.id}`}>
+                  <p>{item.name}</p>
+                </Link>
+                <FavoriteEmptyIcon />
+              </div>
             </div>
-            <div>
-              <Link to={`/${item.id}`}>
-                <p>{item.name}</p>
-              </Link>
-              <FavoriteIcon />
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </section>
-  ) : (
-    <p>Teste</p>
   );
 }
